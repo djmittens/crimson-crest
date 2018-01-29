16bfd84bf22324d1d97640bcba9edacea564ab42
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2018-01-29 01:16:21 -0800

    Cleaning up hello world example.
    will be moving into game loops next.

--- sandbox/src/main/scala/me/ngrid/LwjglHelloWorld.scala
+++ sandbox/src/main/scala/me/ngrid/LwjglHelloWorld.scala
@@ -8,17 +8,18 @@ import org.lwjgl.opengl.GL11._
 //import cats.implicits._
 
 object LwjglHelloWorld {
-  var window: Long = _
-
   def main(args: Array[String]): Unit = {
     println(s"Hello LWJGL ${Version.getVersion}!")
+
     val glfw = GlfwInterpIO
     val gl = LwjglOpenGL11
 
     RunGlfwApp {
       for {
         w <- glfw.createOpenGL()
-        _ <- glfw.renderLoop(w, new SimpleLoopGL(gl)(gl.clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)))
+        _ <- glfw.renderLoop(w, new SimpleLoopGL(gl){
+          gl.clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        })
         _ <- glfw.close(w)
       } yield ()
     }
