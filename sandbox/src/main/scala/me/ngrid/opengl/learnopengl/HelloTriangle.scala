afb0bfc367a7f859b4e5196e90ad24e66aec6f3b
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2018-06-15 20:01:29 -0600

    Rejiggering the file layout for hello triangle
    This is meant to make the intent much clearer and therefore the file
    much cleaner

--- sandbox/src/main/scala/me/ngrid/opengl/learnopengl/HelloTriangle.scala
+++ sandbox/src/main/scala/me/ngrid/opengl/learnopengl/HelloTriangle.scala
@@ -4,12 +4,45 @@ import cats.effect.IO
 import me.ngrid.crimson.api.graphics.RenderLoopAlg
 import me.ngrid.crimson.graphics.lwjgl.opengl.interpreters.{GL20ShaderInterpIO, GLSimpleLoop, GLViewportIO}
 import me.ngrid.opengl.SimpleWindow
-import org.lwjgl.opengl.{GL11, GL15}
+import org.lwjgl.opengl.{GL11, GL15, GLCapabilities}
 
 /**
   * Implementation of Hello Triangle from learn opengl tutorial series.
   */
 object HelloTriangle {
+
+  def main(args: Array[String]): Unit = {
+    SimpleWindow(GLSimpleLoop(
+      gl => RenderLoopAlg.dynamic[IO, State](
+        _init = init(gl),
+        _render = render,
+        _terminate = terminate
+      )
+    )).program.unsafeRunSync()
+    ()
+  }
+
+  def init(gl: GLCapabilities): IO[State] = for {
+    _ <- bg(gl).fold(IO.unit)(_.setBackgroundColor(0.2f, 0.3f, 0.3f, 1.0f))
+    buffer <- IO {
+      val vBuffer = GL15.glGenBuffers()
+      GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vBuffer)
+      GL15.glBufferData(vBuffer, triangleVertices, GL15.GL_STATIC_DRAW)
+      vBuffer
+    }
+  } yield State(glVertexBuffer = buffer, glShaderProgram = 0)
+
+  def render(state: State): IO[Unit] = IO {
+    GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT)
+  }
+
+  def terminate(state: State): IO[Unit] = IO.unit
+
+
+  case class State(
+                    glVertexBuffer: Int,
+                    glShaderProgram: Int
+                  )
   // language=GLSL
   val vertexShader: String =
     """
@@ -21,36 +54,15 @@ void main() {
 }
     """.stripMargin
 
-  def main(args: Array[String]): Unit = {
 
-    //triangle
-    val vertices = Array(
-      -0.5f, -0.5f, 0.0f,
-      0.5f, -0.5f, 0.0f,
-      0.0f, 0.5f, 0.0f
-    )
+  //triangle
+  val triangleVertices = Array(
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+  )
 
-    val bg = GLViewportIO
-    val glsl = GL20ShaderInterpIO
 
-    SimpleWindow(GLSimpleLoop(
-      gl => RenderLoopAlg.dynamic[IO, Int](
-        _init = for {
-          _ <- bg(gl).fold(IO.unit)(_.setBackgroundColor(0.2f, 0.3f, 0.3f, 1.0f))
-          buffer <- IO {
-            val vBuffer = GL15.glGenBuffers()
-            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vBuffer)
-            GL15.glBufferData(vBuffer, vertices, GL15.GL_STATIC_DRAW)
-            vBuffer
-          }
-        } yield buffer,
-
-        _render = _ => IO {
-          GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT)
-        },
-        _terminate = _ => IO.unit
-      )
-    )).program.unsafeRunSync()
-    ()
-  }
+  private val bg = GLViewportIO
+  private val glsl = GL20ShaderInterpIO
 }
