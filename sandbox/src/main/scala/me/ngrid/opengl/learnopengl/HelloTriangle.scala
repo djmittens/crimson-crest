a72fbc381328e16768ae6fe40390a19735c0ac65
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2018-06-16 02:22:32 -0600

    Cool it compiles, but i dont see a triangle... more debugging yay

--- sandbox/src/main/scala/me/ngrid/opengl/learnopengl/HelloTriangle.scala
+++ sandbox/src/main/scala/me/ngrid/opengl/learnopengl/HelloTriangle.scala
@@ -5,7 +5,7 @@ import me.ngrid.crimson.api.graphics.RenderLoopAlg
 import me.ngrid.crimson.graphics.lwjgl.opengl.algebras.GLShaderAlg
 import me.ngrid.crimson.graphics.lwjgl.opengl.interpreters.{GL20ShaderInterpIO, GLSimpleLoop, GLViewportIO}
 import me.ngrid.opengl.SimpleWindow
-import org.lwjgl.opengl.{GL11, GL15, GL20, GLCapabilities}
+import org.lwjgl.opengl.{GL11, GL15, GL20, GL30, GLCapabilities}
 import cats.implicits._
 //import cats.syntax._
 
@@ -27,17 +27,43 @@ object HelloTriangle {
 
   def init(gl: GLCapabilities): IO[State] = for {
     _ <- bg(gl).fold(IO.unit)(_.setBackgroundColor(0.2f, 0.3f, 0.3f, 1.0f))
-    buffer <- createGLVertexBuffer()
+    triangleVao <- createTriangle()
+    program <- createShaderProgram()
+  } yield State(
+    triangleVao = triangleVao,
+    glShaderProgram = program
+  )
+
+  def createShaderProgram(): IO[Either[String, GLShaderAlg.Program[IO]]] = for {
     fShader <- glsl.createShader(glsl.GL_FRAGMENT_SHADER, fragmentShader)
     vShader <- glsl.createShader(glsl.GL_VERTEX_SHADER, vertexShader)
     shaders = List(fShader, vShader)
     //in intellij this is red, (why why intellij?), but it actually compiles!!
-    program <- List(fShader, vShader).sequence.map(glsl.createShaderProgram).sequence
-  } yield State(
-    glVertexBuffer = buffer,
-    glShaders = shaders,
-    glShaderProgram = program.flatten
-  )
+    program <- shaders.sequence.map(glsl.createShaderProgram).sequence
+    _ <- shaders.traverse {
+      case Left(_) => IO.unit
+      case Right(x) => x.delete
+    }
+  } yield program.flatten
+
+  def createTriangle(): IO[Int] = for {
+    vao <- createGLVertexArrayObject()
+    vbo <- createGLVertexBuffer()
+    _ <- IO {
+      //The parameters for this are weird
+      //TODO: review and document all the stuff thats going on in here.
+      //TODO: 3*32 is a magic code for, 3 floats at a time makes a single point, but really we gotta have a better way to describe size of float
+      GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 3 * 32, 0L)
+      //What the heck does this do again?
+      GL20.glEnableVertexAttribArray(0)
+    }
+  } yield vao
+
+  def createGLVertexArrayObject(): IO[Int] = IO {
+    val vao = GL30.glGenVertexArrays()
+    GL30.glBindVertexArray(vao)
+    vao
+  }
 
   def createGLVertexBuffer(): IO[Int] = IO {
     val vBuffer = GL15.glGenBuffers()
@@ -49,14 +75,10 @@ object HelloTriangle {
 
   def render(x: State): IO[Unit] = IO {
     x.glShaderProgram.foreach { program =>
-      //The parameters for this are weird
-      //TODO: review and document all the stuff thats going on in here.
-      GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 3 * 32, 0L)
-      //What the heck does this do again?
-      GL20.glEnableVertexAttribArray(0)
-//      GL20.glUseProgram(program)
+      GL20.glUseProgram(program.ptr)
+      GL30.glBindVertexArray(x.triangleVao)
+      GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, 3)
     }
-
 
 
     GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT)
@@ -65,17 +87,13 @@ object HelloTriangle {
   def terminate(x: State): IO[Unit] = {
     //todo: we can actually delete the shader objects right after we link them into a program, as we wont need them anymore
     //This is a good piece of information, because it means the program is the actual unit that matter for render calls.
-    x.glShaders.traverse {
-      case Left(_) => IO.unit
-      case Right(x) => x.delete
-    } *> x.glShaderProgram.traverse(_.delete) *> IO.unit
+    x.glShaderProgram.traverse(_.delete) *> IO.unit
   }
 
 
   case class State(
-                    glVertexBuffer: Int,
-                    glShaderProgram: Either[String, GLShaderAlg.Program[IO]],
-                    glShaders: List[Either[String, GLShaderAlg.Shader[IO]]]
+                    triangleVao: Int,
+                    glShaderProgram: Either[String, GLShaderAlg.Program[IO]]
                   )
 
 
